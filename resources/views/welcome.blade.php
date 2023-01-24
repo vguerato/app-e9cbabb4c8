a662a1d040d8ca10672de031af0e6059bfd0830f
vinicius <vinicius@slice.global>
2023-01-24 11:48:17 -0300

    Remove grid.js plugin and add table features

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,19 +16,56 @@
                 </div>
             </form>
 
-            <div id="table" class="shadow-none"></div>
+            <input
+                type="text"
+                id="search"
+                class="form-control my-4"
+                oninput="table.search(this.value)"
+                placeholder="Search for some term"
+            />
+
+            <table id="paths-table" class="table">
+                <thead>
+                    <tr>
+                        <td>Path</td>
+                        <td>Value</td>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
         </div>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/gridjs/dist/gridjs.umd.js"></script>
         <script>
-            const grid = new gridjs.Grid({
-                columns: ['Path', 'Value'],
-                data: [],
-                search: true,
-                pagination: true
-            }).render(document.getElementById('table'));
+            const table = {
+                el: document.querySelector('#paths-table tbody'),
+
+                insert(data) {
+                    const row = document.createElement('tr');
+                    for (const key in data) {
+                        const column = document.createElement('td')
+                        column.textContent = data[key]
+                        row.appendChild(column)
+                    }
+
+                    this.el.appendChild(row)
+                },
+
+                clear() {
+                    this.el.innerHTML = null
+                },
+
+                search(term) {
+                    this.el.querySelectorAll('tr').forEach(el => {
+                        if (el.outerHTML.includes(term)) {
+                            el.style.display = 'table-row';
+                        } else {
+                            el.style.display = 'none';
+                        }
+                    })
+                }
+            }
 
             const uploader = document.getElementById('file-picker');
             uploader.onchange = function () {
@@ -40,13 +77,19 @@
                     const data = new FormData();
                     data.append('file', file)
 
-                    fetch('http://localhost:8000/api/import', {
+                    fetch('{{ env('APP_URL') }}:{{ env('APP_PORT') }}/api/import', {
                         method: 'POST',
-                        body: data
+                        body: data,
+                        headers: {
+                            'Accept': 'application/json'
+                        }
                     })
                         .then((response) => response.json())
                         .then((result) => {
-                            grid.updateConfig({ data: result }).forceRender();
+                            if (result) {
+                                table.clear()
+                                result.forEach(data => table.insert(data))
+                            }
                         })
                         .catch((error) => {
                             alert('Error: ' + error);
